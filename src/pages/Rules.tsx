b049f61ba4a4db3512dcb9dba035cf8ee35fde81
LendSpele <151390749+LendSpele@users.noreply.github.com>
2026-04-22 12:43:51 +0200

    Expand prohibited modifications in Rules.tsx

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -54,7 +54,7 @@ const Rules = () => {
                   Играть можно только с модификациями, которые не дают преимущества над игроками или преимущества в развитии.
                   <div className="mt-2 text-sm text-gray-400 bg-black/30 p-3 rounded-lg">
                     <span className="text-green-400 font-bold">Разрешено:</span> Уменьшенный огонь, FullBright, Показ хп, Автокликеры.<br />
-                    <span className="text-red-400 font-bold">Запрещено:</span> X-Ray, AutoSwitch.<br />
+                    <span className="text-red-400 font-bold">Запрещено:</span> X-Ray, AutoSwitch, AutoTotem, Chest Stealer, Baritone, Auto-Trade.<br />
                     <span className="text-gray-500 italic">Если есть вопросы, напишите администратору.</span>
                   </div>
                 </div>
